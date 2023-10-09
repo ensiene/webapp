f2c291c77e611213e316c6fd1b4aea1804ec82a8
Diego Kelme <kelme.dev@pm.me>
2023-10-09 22:37:24 +0000

    v0.1.3.2

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
       <div className='flex align-middle justify-center items-center flex-col'>
         <div className="bg-cover bg-center bg-no-repeat w-screen h-screen flex items-center justify-left" style={{ backgroundImage: 'url(/assets/images/hero.webp)' }}>
           <div className='flex flex-col'>
+            
             <span className='lg:text-5xl text-3xl font-bold my-4 text-left max-w-2xl lg:ml-32 ml-4'> If you want to learn or share what you know, you've come to the right place. </span>
             <span className='lg:text-xl text-md font-normal my-4 text-left max-w-sm lg:ml-32 ml-4'> Discover a world of technology: study, learn, and apply like never before. </span>
 
