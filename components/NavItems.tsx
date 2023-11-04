19b8634bbafd02c6a995821e0fe5f47e6d88af7b
Diego Kelme <kelme.dev@pm.me>
2023-11-04 16:00:27 +0000

    0.1.4.6

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -206,7 +206,7 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link
+        <a
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -218,7 +218,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </Link>
+        </a>
       </NavigationMenuLink>
     </li>
   )
